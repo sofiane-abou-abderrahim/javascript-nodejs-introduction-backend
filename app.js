e12b3e2d2daf2326e46736f97a0a2f91b6fe4c27
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-10-19 22:35:30 +0300

    wrote and read a file with some nodejs apis like fs, writeFile and readFile

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const userName = 'Max';
+const fs = require('fs');
 
-console.log(`Hi ${userName}!`);
+fs.readFile('user-data.txt', (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  // console.log(data);
+  console.log(data.toString());
+});
+
+fs.writeFile('user-data.txt', 'username=Max', err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Wrote to file!');
+  }
+});
