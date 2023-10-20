ce60401279454dad72948063dd7a5e3b738a7bf0
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-10-20 19:30:40 +0300

    extracted data with expressjs and body parser

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+// with Express.js we can use a middleware that does this body parsing for us
+// and the very popular package for parsing the request body and adding the parsed body to the request object is the body-parser package
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   res.setHeader('Content-Type', 'text/html');
@@ -8,7 +13,11 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-  res.send('<h1>Hello World!</h1>'); // new method added by Express.js
+  const userName = req.body.username || 'Unknown User';
+
+  res.send(
+    `<h1>Hi ${userName}</h1><form method="POST" action="/"><input name="username" type="text"><button type="submit">Send</button></form>`
+  ); // new method added by Express.js
 });
 
 app.listen(3000);
