22ca33dc37a2d2be24c064710312f72817581eb2
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-10-21 12:07:46 +0300

    added locId in res.json() to sent it to the frontend

--- routes/location.js
+++ routes/location.js
@@ -7,15 +7,16 @@ const locationStorage = {
 };
 
 router.post('/add-location', (req, res, next) => {
+  const id = Math.random();
   locationStorage.locations.push({
-    id: Math.random(),
+    id: id,
     address: req.body.address,
     coords: {
       lat: req.body.lat,
       lng: req.body.lng
     }
   });
-  res.json({ message: 'Stored location!' });
+  res.json({ message: 'Stored location!', locId: id }); // send back the id in our response so that the client can generate a link with the id in it
 });
 
 router.get('/location', (req, res, next) => {});
