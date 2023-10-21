8831dc7c57bba43cf0373b35de5b33296279073e
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-10-21 14:38:37 +0300

    retrieved the data for a given id via router.get

--- routes/location.js
+++ routes/location.js
@@ -19,6 +19,18 @@ router.post('/add-location', (req, res, next) => {
   res.json({ message: 'Stored location!', locId: id }); // send back the id in our response so that the client can generate a link with the id in it
 });
 
-router.get('/location', (req, res, next) => {});
+// we retrieve the data for a given ID
+router.get('/location/:lid', (req, res, next) => {
+  // we add an extra segment /:lid to the path for which we filter where we tell to ExpressJS that we have a dynamic segment
+  // Express will handle the URL which contains the ID as part of the URL that we get from the frontend
+  const locationId = +req.params.lid; // we retrieve that ID with the "params" that ExpressJS provides to us
+  const location = locationStorage.locations.find(loc => {
+    return loc.id === locationId;
+  });
+  if (!location) {
+    return res.status(404).json({ message: 'Not found!' });
+  }
+  res.json({ address: location.address, coordinates: location.coords });
+});
 
 module.exports = router;
